9a5b3fdbcc245c21685e81bb2ae9553d7fe052cd
ArmandoZ <armandozr@gmail.com>
2021-02-16 22:54:09 -0600

    update

--- src/Array.java
+++ src/Array.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Array {
+
+
     public static void main (String[] args){
-        
+        String nombre;
+        String[] calificaciones = {"F","E","D","C","B","A"};
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del alumno a calificar: ");
+        nombre = teclado.nextLine();
+
+        class promCalif{
+
+        }
+
+
+
+
     }
 }
